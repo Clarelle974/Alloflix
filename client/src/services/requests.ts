aa8987ca4ea3ba19e7967de6498076e59e02e5f9
Julien PICART <julienpicart01@gmail.com>
2025-01-09 16:42:34 +0100

    Add requests file for API

--- client/src/services/requests.ts
+++ client/src/services/requests.ts
@@ -1,1 +1,8 @@
-// Requêtes API
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMTdmM2Y0ODZmMDFhNzZhNGUzMjJlZmI5NTExMmRlZiIsIm5iZiI6MTczNjQxMjIzMS4wMTQwMDAyLCJzdWIiOiI2NzdmOGM0N2M4MWFjYWE2M2RiYjNjYTkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.-wSwEdNS3XemAnW1ZYt0qj1nlil4ccn-w-2MPmVk4Xs",
+  },
+};
