9bbedfbeb976195ac846617516fb1a43ae089c04
Alex Taranne <alextaranne@gmail.com>
2025-01-09 16:14:40 +0100

    US_04 done, add searchbar in header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,6 +4,10 @@ export default function Header() {
   return (
     <header>
       <h1>Bienvenue sur Alloflix</h1>
+      <div className="input">
+        <input type="search" placeholder="Rechercher un film/artiste" />
+        <button type="submit">Rechercher</button>
+      </div>
     </header>
   );
 }
