1e77d14a5dd00a4c780056da7b0e1d778efd08fc
Julien PICART <julienpicart01@gmail.com>
2025-01-10 10:22:18 +0100

    .env OK + fixe API request

--- client/src/services/requests.ts
+++ client/src/services/requests.ts
@@ -2,7 +2,6 @@ const options = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMTdmM2Y0ODZmMDFhNzZhNGUzMjJlZmI5NTExMmRlZiIsIm5iZiI6MTczNjQxMjIzMS4wMTQwMDAyLCJzdWIiOiI2NzdmOGM0N2M4MWFjYWE2M2RiYjNjYTkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.-wSwEdNS3XemAnW1ZYt0qj1nlil4ccn-w-2MPmVk4Xs",
+    Authorization: process.env.KEY_API,
   },
 };
