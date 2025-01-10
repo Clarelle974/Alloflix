21afd417fe4a23331f8ddfadb40b7ded4a69060a
Julien PICART <julienpicart01@gmail.com>
2025-01-10 12:40:28 +0100

    api request progress

--- client/src/services/requests.ts
+++ client/src/services/requests.ts
@@ -1,7 +1,9 @@
 const options = {
   method: "GET",
+  url: `${process.env.BASE_URL}`,
+  params: { language: "en-FR", page: "1" },
   headers: {
     accept: "application/json",
-    Authorization: process.env.KEY_API,
+    Authorization: process.env.API_KEY,
   },
 };
