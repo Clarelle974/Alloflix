f836f0cf7172e30238c617d4612abbd9c143e066
Julien PICART <julienpicart01@gmail.com>
2025-01-10 17:11:58 +0100

    fixe api key

--- client/src/services/requests.ts
+++ client/src/services/requests.ts
@@ -2,17 +2,19 @@ import axios from "axios";
 
 const options = {
   method: "GET",
-  url: import.meta.env.BASE_URL,
-  params: { language: "en-FR", page: "1" },
+  params: { language: "en-US", page: "1" },
   headers: {
     accept: "application/json",
-    Authorization: import.meta.env.API_KEY,
+    Authorization: `Bearer ${import.meta.env.API_KEY}`,
   },
 };
 
 const getPopularMovies = () => {
   return axios
-    .get("https://api.themoviedb.org/3/movie/popular", options)
+    .get(
+      "https://api.themoviedb.org/3/movie/popular?api_key=4af27d13c2c0f2c8cd1714e402ac5add",
+      options,
+    )
     .then((response) => response.data.results)
     .catch((error) => console.error(error));
 };
