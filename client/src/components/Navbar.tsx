655a9dbebf2b156c50295b10783ab0ae378e9253
Lisa BELLALI <lisa.bellali92@gmail.com>
2025-01-14 16:24:01 +0100

    ajout lien accueil logo

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -25,10 +25,12 @@ export default function Navbar() {
   return (
     <nav>
       <div className="logo">
-        <img
-          src="https://cdn.discordapp.com/attachments/1324658166461370428/1326553270038171678/Alloflix_logo.png?ex=677fd881&is=677e8701&hm=ff1c4aa91b43bd28ac960affe08cebb6b4bceb9734328c214cdc948f0a7ff2e1&"
-          alt="Alloflix Logo"
-        />
+        <Link to="/">
+          <img
+            src="https://cdn.discordapp.com/attachments/1324658166461370428/1326553270038171678/Alloflix_logo.png?ex=677fd881&is=677e8701&hm=ff1c4aa91b43bd28ac960affe08cebb6b4bceb9734328c214cdc948f0a7ff2e1&"
+            alt="Alloflix Logo"
+          />
+        </Link>
         <ul>
           <li>
             <Link to="/">Accueil</Link>
