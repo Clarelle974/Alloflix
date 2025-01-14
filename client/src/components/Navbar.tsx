072843a923af757a4a4b8ff6c92e002cdab4759c
Lisa BELLALI <lisa.bellali92@gmail.com>
2025-01-14 16:15:35 +0100

    ajouter liste déroulante

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/navbar.css";
 
 export default function Navbar() {
+  const [isMoviesDropdownVisible, setMoviesDropdownVisible] = useState(false);
+  const [isArtistsDropdownVisible, setArtistsDropdownVisible] = useState(false);
+
+  const handleMoviesMouseEnter = () => {
+    setMoviesDropdownVisible(true);
+  };
+
+  const handleMoviesMouseLeave = () => {
+    setMoviesDropdownVisible(false);
+  };
+
+  const handleArtistsMouseEnter = () => {
+    setArtistsDropdownVisible(true);
+  };
+
+  const handleArtistsMouseLeave = () => {
+    setArtistsDropdownVisible(false);
+  };
+
   return (
     <nav>
       <div className="logo">
@@ -13,11 +33,40 @@ export default function Navbar() {
           <li>
             <Link to="/">Accueil</Link>
           </li>
-          <li>
+          <li
+            className="dropdown"
+            onMouseEnter={handleMoviesMouseEnter}
+            onMouseLeave={handleMoviesMouseLeave}
+          >
             <Link to="/movies">Films</Link>
+            {isMoviesDropdownVisible && (
+              <ul className="dropdown-menu">
+                <li>
+                  <Link to="/movies/populaires">Populaires</Link>
+                </li>
+                <li>
+                  <Link to="/movies/dumoment">Du moment</Link>
+                </li>
+                <li>
+                  <Link to="/movies/avenir">À venir</Link>
+                </li>
+              </ul>
+            )}
           </li>
-          <li>
+          <li
+            className="dropdown"
+            onMouseEnter={handleArtistsMouseEnter}
+            onMouseLeave={handleArtistsMouseLeave}
+          >
             <Link to="/artists">Artistes</Link>
+            {isArtistsDropdownVisible && (
+              <ul className="artists-dropdown-menu">
+                <li>
+                  <Link to="/artists/acteurs">Acteurs</Link>
+                  <Link to="/artists/realisateurs">Réalisateurs</Link>
+                </li>
+              </ul>
+            )}
           </li>
         </ul>
       </div>
