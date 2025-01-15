c8cf8c8f02532b03ad7cffba99171514d4261ef8
Clarelle974 <clarelle.lemonnier@gmail.com>
2025-01-15 15:55:57 +0100

    fix PR

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -2,15 +2,6 @@ import Header from "../components/Header";
 import PopularMovie from "../components/PopularMovie";
 import Trailer from "../components/Trailer";
 
-// interface MovieTypes {
-//   id: number;
-//   backdrop_path: string;
-//   title: string;
-//   poster_path: string;
-//   vote_average: number;
-//   release_date: string;
-// }
-
 export default function Homepage() {
   return (
     <>
