6278dd981602abcffc965d0144e5e3f4d0cc48fa
Clarelle974 <clarelle.lemonnier@gmail.com>
2025-01-20 09:23:58 +0100

    brouillon

--- client/src/pages/Search.tsx
+++ client/src/pages/Search.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+// import { useLoaderData, useNavigate, useParams } from "react-router-dom";
 import { useLoaderData, useParams } from "react-router-dom";
 import MovieCard from "../components/MovieCard";
 
@@ -14,13 +16,29 @@ interface ResultsTypes {
 export default function Search() {
   const { movie } = useParams();
   const results = useLoaderData() as ResultsTypes[];
+  const [searchedMovie, setSearchedMovie] = useState("");
+  // const navigate = useNavigate();
+  const handleChangeSearchBar = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setSearchedMovie(event.currentTarget.value);
+  };
+  // const sendSearchedMovie = (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   navigate(`/search/${searchedMovie}`);
+  // };
+  // mettre un ecouteur touche entrée
 
   return (
     <>
       <div className="search">
         <div>
           <img src="/src/assets/images/search-icon.png" alt="search" />
-          <p>{movie}</p>
+          <input
+            placeholder={movie}
+            value={searchedMovie}
+            onChange={handleChangeSearchBar}
+          />
         </div>
         <div className="search-results">
           {results.map((movie) => (
