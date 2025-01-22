b4f0597ee022cedf74d3060f676b55449f7b4a89
Julien PICART <julienpicart01@gmail.com>
2025-01-22 15:56:26 +0100

    Change background header + join us

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -14,8 +14,8 @@ export default function Homepage() {
   const { popular } = useLoaderData() as {
     popular: Homepage[];
   };
-  const backgroundImg = popular[0].backdrop_path;
-  const backgroundImgJoinUs = popular[1].backdrop_path;
+  const backgroundImg = popular[1].backdrop_path;
+  const backgroundImgJoinUs = popular[2].backdrop_path;
   return (
     <>
       <Header backgroundImg={backgroundImg} />
