db0b34f06d2bbdd15551bec16352762289e1fd69
Clarelle974 <clarelle.lemonnier@gmail.com>
2025-01-24 12:42:37 +0100

    US fonctionnelle

--- client/src/types/index.d.ts
+++ client/src/types/index.d.ts
@@ -1,22 +1,21 @@
-//TrailerCard, Trailer, MovieCard
-interface MovieDataTypes {
-  movie: MovieTypes;
+interface ArticleTypes {
+  id: number;
+  media_type: string;
+  original_title: string;
+  poster_path: string;
+  profile_path: string;
+  popularity: number;
+  backdrop_path: string | null;
 }
 
 //from Movies, PopularMovies, TheaterMovies, UpcomingMovies, Search
-interface MovieTypes {
-  id: number;
+interface MovieTypes extends ArticleTypes {
   title: string;
-  backdrop_path: string | null;
   budget: number;
   genres: { id: number; name: string }[];
   genre_ids: number[];
-  media_type: string;
-  original_title: string;
   origin_country: string;
   overview: string;
-  poster_path: string;
-  profile_path: string;
   tagline: string;
   release_date: string;
   runtime: number;
@@ -24,22 +23,11 @@ interface MovieTypes {
   vote_average: number;
   release_date: string;
 }
-
-interface ResultsTypes {
-  id: number;
-  title: string;
-  backdrop_path: string | null;
-  budget: number;
-  genres: { id: number; name: string }[];
-  genre_ids: number[];
-  media_type: string;
-  original_title: string;
-  origin_country: string;
-  overview: string;
-  poster_path: string;
-  profile_path: string;
-  tagline: string;
-  release_date: string;
+//TrailerCard, Trailer, MovieCard
+interface MovieDataTypes {
+  movie: MovieTypes;
+}
+interface ResultsTypes extends MovieTypes {
   runtime: number;
   videos: Videos;
   vote_average: number;
@@ -50,39 +38,29 @@ interface ResultsTypes {
   deathday: string;
   name: string;
   original_name: string;
-  media_type: string;
   adult: boolean;
-  popularity: number;
   gender: number;
   known_for_department: string;
-  original_title: string;
   place_of_birth: string;
-  poster_path: string;
-  profile_path: string;
 }
+
 //Homepage
 interface Background {
   backdrop_path: string;
 }
 
 // Artists, ArtistsDetails, ArtistDetails, MovieDetails, Filmograhy
-interface ArtistTypes {
-  id: number;
+interface ArtistTypes extends ArticleTypes {
   birthday: string;
   biography: string;
   character: string;
   deathday: string;
   name: string;
   original_name: string;
-  media_type: string;
   adult: boolean;
-  popularity: number;
   gender: number;
   known_for_department: string;
   place_of_birth: string;
-  poster_path: string;
-  profile_path: string;
-  original_title: string;
 }
 
 //ArtistCards
